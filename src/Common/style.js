ef1d926fb29f8c3010b6cb449d22bc78d44cee48
beafshar <baharafshar2079@gmail.com>
2022-04-25 00:41:32 +0430

    created folders and added css files.

---
+++ src/Common/style.js
@@ -0,0 +1,12 @@
+import logo from '../template.png';
+import './style.css';
+
+
+function Header() {
+    return (
+        <div className="topnav">
+            <a href="#" className="align-left"><img className="align-left" src="pictures/template.png" alt="logo"></a>
+
+        </div>
+    );
+}
